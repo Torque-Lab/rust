e1b99f3f2227b7c7202ab4eb89dfc83e6b97a7fa
Mathura <academic1mathura@gmail.com>
2025-08-01 12:34:39 +0000

    ownership and brrower

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -12,7 +12,17 @@ fn _fact2(i: i128) -> i128 {
         return i * _fact2(i - 1);
     }
 }
+
+fn give_length(s: &String) -> usize {
+    s.len()
+}
+
 fn main() {
+    let str: String = String::from("Hello, world!");
+    let len: usize = give_length(&str);
+    println!("Length of string: {}", len);
+
+    println!("{}", str);
     println!("Hello, world!");
     let res: u128 = fact(20);
     println!("{}", res);
