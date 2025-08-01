74e4e9f55acb43159474e7da8f5267606d1e953c
Mathura <academic1mathura@gmail.com>
2025-08-01 06:58:09 +0000

    u and i data type

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,3 +1,19 @@
+fn fact(i: u128) -> u128 {
+    if i == 0 {
+        return 1;
+    } else {
+        return i * fact(i - 1);
+    }
+}
+fn _fact2(i: i128) -> i128 {
+    if i == 0 {
+        return 1;
+    } else {
+        return i * _fact2(i - 1);
+    }
+}
 fn main() {
     println!("Hello, world!");
+    let res: u128 = fact(20);
+    println!("{}", res);
 }
